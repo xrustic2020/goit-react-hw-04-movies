3db15617d158583100cf4878c4973056e4322cac
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2021-04-27 15:11:15 +0300

    changed git ignore settings

--- src/API/settings.js
+++ src/API/settings.js
@@ -1,41 +1,35 @@
 import axios from 'axios';
+import key from 'apiKey';
 
-const KEY = '3fb33685800e055b072f7d1be89729ed';
 const baseImgUrl = 'https://image.tmdb.org/t/p/original';
-
-// fetch(
-//   'https://api.themoviedb.org/3/search/movie?api_key=3fb33685800e055b072f7d1be89729ed&query=batman&page=1',
-// )
-//   .then(res => res.json())
-//   .then(data => console.log(data));
 
 async function searchFilms(query) {
   return axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${query}&page=1`,
+    `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}&page=1`,
   );
 }
 
 async function getTrendingFilms() {
   return axios.get(
-    `https://api.themoviedb.org/3/trending/movie/day?api_key=${KEY}`,
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}`,
   );
 }
 
 async function getOneFilmDetails(id) {
   return axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language=en-US`,
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=en-US`,
   );
 }
 
 async function getFilmCast(id) {
   return axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${KEY}`,
+    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${key}`,
   );
 }
 
 async function getFilmReviews(id) {
   return axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${KEY}`,
+    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${key}`,
   );
 }
 
